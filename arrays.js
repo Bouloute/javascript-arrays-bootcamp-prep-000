a37e43fe68d6017080426b7397725eb5cf637b76
Annaick Lewis Dagnino <annaick1995@gmail.com>
2019-11-07 17:05:25 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -61,7 +61,7 @@ function removeElementFromBeginningOfArray(array){
 //Removing an element at the end of an array___________________
 function destructivelyRemoveElementFromEndOfArray(array){
   
-  array.shift(array.length)
+  array.unshift(array.length)
   
   return array
 }
