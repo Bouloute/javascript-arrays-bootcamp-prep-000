00e1963e89a734359c7cf0dbaf63d649f0491e9a
Annaick Lewis Dagnino <annaick1995@gmail.com>
2019-11-07 16:39:31 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,15 @@
+var chocolateBars= ['snickers', 'hundred grand ', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, element) {
+  
+  var newArray = [ element, ...array]
+
+  return( newArray)
+
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+
+ array = array.unshift(element)
+ return array
+}
